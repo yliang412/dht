72e4b8c6e20c90480688c26864907565bfad3205
Yuchen Liang <yuchenl3@andrew.cmu.edu>
2023-04-29 00:55:46 -0400

    feat: implement single server kv store
    Signed-off-by: Yuchen Liang <yuchenl3@andrew.cmu.edu>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #[tarpc::service]
 pub trait HashNode {
-    async fn hello(name: String) -> String;
     async fn get(key: String) -> Option<String>;
     async fn insert(key: String, value: String) -> Option<String>;
     async fn remove(key: String) -> Option<String>;
